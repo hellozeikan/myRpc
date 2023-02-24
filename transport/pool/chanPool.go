679b32dff7a72ee93e3c437266464951c3a14a9e
chen <970561372@qq.com>
2023-02-24 17:57:43 +0800

    Perfect connection pool

--- transport/pool/chanPool.go
+++ transport/pool/chanPool.go
@@ -1,15 +1,25 @@
 package pool
 
 import (
+	"context"
 	"errors"
+	"io"
 	"net"
 	"sync"
+	"sync/atomic"
 	"time"
 )
 
 type chanPool struct {
-	mu    sync.Mutex
-	conns chan *Conn
+	mu          sync.Mutex
+	conns       chan *Conn
+	idleTimeout time.Duration
+	dialTimeout time.Duration
+	Dial        func(context.Context) (net.Conn, error)
+	initCap     int
+	maxCap      int
+	maxIdle     int
+	inflight    int32
 }
 
 func (ch *chanPool) Put(conn *Conn) error {
@@ -26,12 +36,78 @@ func (ch *chanPool) Put(conn *Conn) error {
 	case ch.conns <- conn:
 		return nil
 	default:
-		return conn.conn.Close()
+		return conn.Conn.Close()
 	}
+}
+
+func (ch *chanPool) Get(ctx context.Context) (*Conn, error) {
+	if ch.conns == nil {
+		return nil, errors.New("connection closed")
+	}
+	select {
+	case conn := <-ch.conns:
+		if conn == nil {
+			return nil, errors.New("connection closed")
+		}
+		if conn.unable {
+			return nil, errors.New("connection closed")
+		}
+		return conn, nil
+	case <-ctx.Done():
+		return nil, ctx.Err()
+	default:
+		if ch.inflight > int32(ch.maxCap) {
+			select {
+			case conn := <-ch.conns:
+				if conn == nil {
+					return nil, errors.New("connection closed")
+				}
+
+				if conn.unable {
+					return nil, errors.New("connection closed") // 这里出错了没有自动重试，调用方根据错误类型来决定是否重试
+				}
+
+				return conn, nil
+			case <-ctx.Done(): // context取消或超时，则退出
+				return nil, ctx.Err()
+			}
+		}
+		conn, err := ch.Dial(ctx)
+		if err != nil {
+			return nil, err
+		}
+		atomic.AddInt32(&ch.inflight, 1)
+		return ch.wrapConn(conn), nil
+	}
+}
+
+func (ch *chanPool) wrapConn(conn net.Conn) *Conn {
+	p := &Conn{
+		ch:          ch,
+		t:           time.Now(),
+		dialTimeout: ch.dialTimeout,
+	}
+	return p
+}
+
+func (ch *chanPool) Check(cn *Conn) bool {
+	// check timeout
+	if cn.t.Add(ch.idleTimeout).Before(time.Now()) {
+		return false
+	}
+	if !isConnAlive(cn.Conn) {
+		return false
+	}
+	return true
 }
 
 func isConnAlive(conn net.Conn) bool {
 	conn.SetReadDeadline(time.Now().Add(time.Millisecond))
+	buff := make([]byte, 1)
+	if n, err := conn.Read(buff); n > 0 || err == io.EOF {
+		return false
+	}
+	_ = conn.SetReadDeadline(time.Time{})
 
 	return true
 }
--- transport/pool/conn.go
+++ transport/pool/conn.go
@@ -1,6 +1,7 @@
 package pool
 
 import (
+	"errors"
 	"log"
 	"net"
 	"sync"
@@ -8,7 +9,7 @@ import (
 )
 
 type Conn struct {
-	conn        net.Conn
+	Conn        net.Conn
 	ch          *chanPool
 	unable      bool
 	mu          sync.Mutex
@@ -25,14 +26,39 @@ func (p *Conn) Makeunable() {
 func (p *Conn) Close() error {
 	p.mu.Lock()
 	defer p.mu.Unlock()
-	// if p.unable {
-	// 	if p.conn !=nil {
-	// 		return
-	// 	}
-	// }
-	err := p.conn.SetDeadline(time.Time{})
+	if p.unable {
+		if p.Conn != nil {
+			return p.Conn.Close()
+		}
+	}
+	err := p.Conn.SetDeadline(time.Time{})
 	if err != nil {
 		log.Fatalf("SetDeadline error: %v\n", err)
 	}
 	return p.ch.Put(p)
+}
+func (p *Conn) Read(b []byte) (int, error) {
+	// 判断该连接状态
+	if p.unable {
+		return 0, errors.New("connection closed")
+	}
+	n, err := p.Conn.Read(b)
+	if err != nil {
+		p.Makeunable()
+		// 关闭连接
+		p.Conn.Close()
+	}
+	return n, err
+}
+
+func (p *Conn) Write(b []byte) (int, error) {
+	if p.unable {
+		return 0, errors.New("connection closed")
+	}
+	n, err := p.Conn.Write(b)
+	if err != nil {
+		p.Makeunable()
+		p.Conn.Close()
+	}
+	return n, err
 }
