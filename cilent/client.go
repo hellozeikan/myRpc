ac3bc0c4e4e2cdf61917cd7c114d3d27825e1f8c
chen <970561372@qq.com>
2023-03-20 18:01:40 +0800

    Improve the client

--- cilent/client.go
+++ cilent/client.go
@@ -3,11 +3,15 @@ package cilent
 import (
 	"context"
 	"math"
+	"myrpc/code"
 	"myrpc/interceptor"
+	"myrpc/pool"
 	"myrpc/protocol"
 
-	// "myrpc/transport"
+	"myrpc/transport"
 	"strconv"
+
+	"github.com/goinggo/mapstructure"
 	// "github.com/goinggo/mapstructure"
 )
 
@@ -76,72 +80,67 @@ func (c *defaultClient) Invoke(ctx context.Context, req, rsp interface{}, path s
 	return interceptor.ClientIntercept(ctx, req, rsp, c.opts.interceptors, c.invoke)
 }
 func (c *defaultClient) invoke(ctx context.Context, req, rsp interface{}) error {
-	return nil
+	serialization := code.DefaultSerialization
+	arr := make([]interface{}, 0)
+	r := req.(*protocol.Request)
+	arr = append(arr, r.MsgId)
+	arr = append(arr, r.Type)
+	arr = append(arr, r.Method)
+	arr = append(arr, r.Params)
+	payload, err := serialization.Marshal(arr)
+
+	if err != nil {
+		return code.NewFrameworkError(code.ClientMsgErrorCode, "request marshal failed ...")
+	}
+
+	// 添加包头
+	clientCodec := code.DefaultCodec
+	reqbody, err := clientCodec.Encode(payload)
+	if err != nil {
+		return err
+	}
+
+	// 发送请求
+	clientTransport := c.NewClientTransport()
+	clientTransportOpts := []transport.ClientTransportOption{
+		transport.WithServiceName(c.opts.serviceName),
+		transport.WithClientTarget(c.opts.target),
+		transport.WithClientNetwork("tcp"),
+		transport.WithClientPool(pool.GetPool("default")),
+		transport.WithTimeout(c.opts.timeout),
+		// transport.WithSelector(selector.GetSelector(c.opts.selectorName)),
+	}
+	frame, err := clientTransport.Send(ctx, reqbody, clientTransportOpts...)
+	if err != nil {
+		return err
+	}
+
+	// 对 server 回包进行解包
+	rspbuf, err := clientCodec.Decode(frame)
+	if err != nil {
+		return err
+	}
+
+	respp := make([]interface{}, 0)
+	err = serialization.Unmarshal(rspbuf, &respp)
+	if err != nil {
+		return err
+	}
+
+	if respp[1].(string) == "error" {
+		// todo: to rpc error
+		e := protocol.RpcError{}
+		err = mapstructure.Decode(respp[len(respp)-1], &e)
+		if err != nil {
+			return err
+		}
+		return e
+	}
+
+	// 转结构体
+	return mapstructure.Decode(respp[len(respp)-1], &rsp)
 }
 
-// func (c *defaultClient) invoke(ctx context.Context, req, rsp interface{}) error {
-// 	// 对请求体序列化
-// 	serialization := code.DefaultSerialization
-// 	arr := make([]interface{}, 0)
-// 	r := req.(*protocol.Request)
-// 	arr = append(arr, r.MsgId)
-// 	arr = append(arr, r.Type)
-// 	arr = append(arr, r.Method)
-// 	arr = append(arr, r.Params)
-// 	payload, err := serialization.Marshal(arr)
-
-// 	if err != nil {
-// 		return code.NewFrameworkError(code.ClientMsgErrorCode, "request marshal failed ...")
-// 	}
-
-// 	// 添加包头
-// 	clientCodec := code.DefaultCodec
-// 	reqbody, err := clientCodec.Encode(payload)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	// 发送请求
-// 	clientTransport := c.NewClientTransport()
-// 	clientTransportOpts := []transport.ClientTransportOption{
-// 		transport.WithServiceName(c.opts.serviceName),
-// 		transport.WithClientTarget(c.opts.target),
-// 		transport.WithClientNetwork("tcp"),
-// 		transport.WithClientPool(connpool.GetPool("default")),
-// 		transport.WithTimeout(c.opts.timeout),
-// 		transport.WithSelector(selector.GetSelector(c.opts.selectorName)),
-// 	}
-// 	frame, err := clientTransport.Send(ctx, reqbody, clientTransportOpts...)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	// 对 server 回包进行解包
-// 	rspbuf, err := clientCodec.Decode(frame)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	respp := make([]interface{}, 0)
-// 	err = serialization.Unmarshal(rspbuf, &respp)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	if respp[1].(string) == "error" {
-// 		// todo: to rpc error
-// 		e := protocol.RpcError{}
-// 		err = mapstructure.Decode(respp[len(respp)-1], &e)
-// 		if err != nil {
-// 			return err
-// 		}
-// 		return e
-// 	}
-
-// 	// 转结构体
-// 	return mapstructure.Decode(respp[len(respp)-1], &rsp)
-// }
-
-// func (c *defaultClient) NewClientTransport() transport.ClientTransport {
-// 	return transport.DefaultClientTransport
-// }
+func (c *defaultClient) NewClientTransport() transport.ClientTransport {
+	return transport.DefaultClientTransport
+}
